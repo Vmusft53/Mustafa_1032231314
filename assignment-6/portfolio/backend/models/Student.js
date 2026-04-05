5ecf5348e9b3a76bc011a5d9357b8ec50ae17d23
Vmustf53 <mustafavakil01@gmail.com>
2026-04-05 20:32:29 +0530

    Clean assignment-5 branch

--- assignment-6/portfolio/backend/models/Student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const StudentSchema = new mongoose.Schema({
-  name:   { type: String, required: true },
-  email:  { type: String, required: true },
-  course: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Student', StudentSchema);
